f75cfef3465c2b2d87fbbdd0795fc3219d2ddb0a
Julius Cesar <mail@xocade.com>
2024-01-28 15:14:53 +0400

    Started Calculatrice in C

---
+++ c/calculatrice.c
@@ -0,0 +1,26 @@
+/*
+    Calculatrice
+    Written by knznsmn on January 28, 2024
+    
+    This program accepts input and perform calculations on them
+    based on supplied operators.
+
+*/
+
+#include <stdio.h>
+
+void greetings();
+
+int main() {
+
+    greetings();
+    
+    
+    return 0;
+
+
+}
+
+void greetings() {
+    puts("Welcome to Calculatrice!");
+}
