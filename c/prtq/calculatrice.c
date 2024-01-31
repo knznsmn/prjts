623143ec45adb19d7085f569c6353010d55c4dff
Julius C. Cesar <knznsmn@icloud.com>
2024-01-31 18:23:12 +0400

    Small steps.

--- c/prtq/calculatrice.c
+++ c/prtq/calculatrice.c
@@ -10,17 +10,25 @@
 #include <stdio.h>
 
 void greetings();
-
+int isdigit(int);
 int main() {
-
+    
+    char ch;
     greetings();
-    
-    
+    ch = getchar();
+    printf("%i, %c, %i ", ch, ch, isdigit(ch));
+   
     return 0;
-
-
 }
 
 void greetings() {
     puts("Welcome to Calculatrice!");
+}
+
+int isdigit(int c) {
+    if (c >= 48 && c <= 57) {
+        return 1;
+    }
+    else
+        return 0;
 }
