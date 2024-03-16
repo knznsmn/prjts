c03b6c9008c376594b429d716dec8aae26528813
Julius C. Cesar <knznsmn@icloud.com>
2024-03-16 12:37:28 +0400

    Day 001 project.

---
+++ py/001j/main.py
@@ -0,0 +1,7 @@
+# Day 001 of 100 Days of Python
+
+print("Welcome to the Band Name Generator")
+city = input("What's the name of the city you grew up in? ")
+pet = input("What's the name of your favorite pet? ")
+
+print("Your cool band name should be " + city + " " + pet + ".")
